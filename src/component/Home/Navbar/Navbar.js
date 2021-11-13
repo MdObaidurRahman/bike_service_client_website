268513821be0cdc3fe4ce7399938d8f8467e1e46
MdObaidurrahman <obaidur.rahman335.bd@gmail.com>
2021-11-13 19:30:59 +0600

    sesh

--- src/component/Home/Navbar/Navbar.js
+++ src/component/Home/Navbar/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = () => {
                             <Link>Contact</Link>
                         </li>
                         <li>
-                            <Link to='/login'>{loggedInUser.email ? <Link to='/home' className='position' onClick={() => setLoggedIn('')}>Sign Out</Link> : 'Sing In'}</Link>
+                            <Link to='/login'>{loggedInUser.email ? <Link to='/home' className='position' onClick={() => setLoggedIn('')}>Sign Out</Link> : 'Sign In'}</Link>
                             <span className='text-white'>Signed in as:</span> <a href="#login">{loggedInUser?.displayName}</a>
                         </li>
                     </ul>
